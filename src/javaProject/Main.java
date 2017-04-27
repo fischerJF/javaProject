92026f4b44cc76cadf83a768cfff2c854bb6b75d
fischerJF <fischebatera@hotmail.com>
2017-04-27 16:11:24 -0300

    criação do projeto

---
+++ src/javaProject/Main.java
@@ -0,0 +1,13 @@
+package javaProject;
+
+import java.util.logging.Logger;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Logger logger = Logger.getLogger("lopes");
+		  logger.info("iniciando aplicação");
+	}
+
+}
